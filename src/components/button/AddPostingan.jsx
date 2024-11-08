c853722a18575501bd17c31ee36e928bb70629eb
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-11-08 19:52:44 +0700

    Menambahkan komponen button tambah postingan

---
+++ src/components/button/AddPostingan.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react'
+import { FaSquarePlus } from 'react-icons/fa6'
+
+function AddPostingan({ onClickHandler }) {
+    <button>
+        <FaSquarePlus />
+    </button>
+}
+
+export { AddPostingan }

3728f4e75f662b26711d468cc065f52bb3ef5045
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-11-09 14:41:32 +0700

    Menggunakan Froala sebagai modal

--- src/components/modal box/TambahPostingan.jsx
+++ src/components/modal box/TambahPostingan.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { AddPostingan } from '../button/AddPostingan'
+import FroalaEditorComponent from 'react-froala-wysiwyg';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/js/froala_editor.pkgd.min.js';
+
+
 import {
     Dialog,
     Card,
@@ -16,6 +21,8 @@ class TambahPostingan extends Component {
         super(props);
         this.state = {
             open: false,
+            postingan: "",
+            urlGambar: ""
         };
 
         this.handleOpen = this.handleOpen.bind(this);
@@ -26,8 +33,6 @@ class TambahPostingan extends Component {
     };
 
     render() {
-
-
         return (
             <>
                 <AddPostingan onClickHandler={this.handleOpen} />
@@ -37,47 +42,9 @@ class TambahPostingan extends Component {
                     handler={this.handleOpen}
                     className="bg-transparent shadow-none"
                 >
-                    <Card className="mx-auto w-full max-w-[24rem]">
-                        <CardBody className="flex flex-col gap-4">
-                            <Typography variant="h4" color="blue-gray">
-                                Sign In
-                            </Typography>
-                            <Typography
-                                className="mb-3 font-normal"
-                                variant="paragraph"
-                                color="gray"
-                            >
-                                Enter your email and password to Sign In.
-                            </Typography>
-                            <Typography className="-mb-2" variant="h6">
-                                Your Email
-                            </Typography>
-                            <Input label="Email" size="lg" />
-                            <Typography className="-mb-2" variant="h6">
-                                Your Password
-                            </Typography>
-                            <Input label="Password" size="lg" />
-                            <div className="-ml-2.5 -mt-3">
-                                <Checkbox label="Remember Me" />
-                            </div>
-                        </CardBody>
-                        <CardFooter className="pt-0">
-                            <AddPostingan onClick={this.handleOpen} />
-                            <Typography variant="small" className="mt-4 flex justify-center">
-                                Don&apos;t have an account?
-                                <Typography
-                                    as="a"
-                                    href="#signup"
-                                    variant="small"
-                                    color="blue-gray"
-                                    className="ml-1 font-bold"
-                                    onClick={this.handleOpen}
-                                >
-                                    Sign up
-                                </Typography>
-                            </Typography>
-                        </CardFooter>
-                    </Card>
+
+                    <FroalaEditorComponent tag="textarea" />
+
                 </Dialog>
             </>
         );

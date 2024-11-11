b154416a48329feca92fccfeacc3ac326d937b1d
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-11-11 11:43:15 +0700

    Finishing Card Pengguna

--- src/components/card/CardPengguna.jsx
+++ src/components/card/CardPengguna.jsx
@@ -9,14 +9,14 @@ import {
 
 export function CardPengguna() {
     return (
-        <Card className="mt-6 w-60">
-            <Avatar src="https://docs.material-tailwind.com/img/face-2.jpg" alt="avatar" size="xxl" className="m-auto pt-2" />
-            <CardBody className="pb-2">
+        <Card className="mt-6 w-60 max-h-[]">
+            <Avatar src="https://docs.material-tailwind.com/img/face-2.jpg" alt="avatar" size="xxl" className="m-auto mt-2" />
+            <CardBody className="pb-2 pt-3">
                 <Typography variant="h5" color="blue-gray" className="mb-2 text-center">
                     Muhammad Dzaky Raihan
                 </Typography>
-                <Typography className="">
-                    Hi, Im a professional Web Dev
+                <Typography className="text-sm">
+                    Lorem ipsum dolor sit amet consectetur
                 </Typography>
             </CardBody>
             <CardFooter className="pt-0 pb-2 justify-center flex">

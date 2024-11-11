7c7af07314ccc245d40317da1ba988f444ab0883
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-11-11 12:59:44 +0700

    Membuat tabs di halaman follow

--- src/pages/HalamanFollow.jsx
+++ src/pages/HalamanFollow.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Sidebar } from '../components/sidebar/Sidebar'
 import { PenggunaList } from '../components/cardList/PenggunaList'
+import { TabsFollow } from '../components/tabs/TabsFollow'
 
 function HalamanFollow() {
     return (
         <div className='flex bg-blue-gray-50 min-h-[100vh] '>
             <Sidebar />
             <div className='mx-auto flex-grow'>
-                <PenggunaList />
+                {/* <PenggunaList /> */}
+                <TabsFollow />
             </div>
         </div>
     )
 }
 
 export { HalamanFollow }
+
+

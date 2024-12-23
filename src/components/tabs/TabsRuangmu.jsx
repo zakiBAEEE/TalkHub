796766487d4aa7e2f642e90b3cac5ce3ffec3a6d
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-12-23 10:25:57 +0700

    Styling tabs

--- src/components/tabs/TabsRuangmu.jsx
+++ src/components/tabs/TabsRuangmu.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { Tab, Tabs, TabsBody, TabsHeader } from '@material-tailwind/react'
 
 
-function TabsRuangmu({ handler, stateAktif, pencarianHandler }) {
+function TabsRuangmu({ handler, stateAktif }) {
     return (
-        <Tabs value={stateAktif} className=" m-auto mt-2 flex flex-col gap-4 items-center flex-grow w-[90%]">
+        <Tabs value={stateAktif} className=" m-auto mt-2 flex flex-col gap-4 items-start flex-grow w-[90%]">
             <TabsHeader
-                className="rounded-none border-b border-blue-gray-50 bg-transparent p-0 gap-5 w-full flex flex-row justify-center items-center w-96"
+                className="rounded-none border-b border-blue-gray-50 bg-transparent p-0 gap-5 w-full flex flex-row justify-center items-center w-56"
                 indicatorProps={{
                     className:
                         "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",
                 }}
             >
                 <Tab key='cari' value='cari' onClick={() => { handler('cari') }} className='w-full'>Mengikuti</Tab >
+
                 <Tab key='mengikuti' value='mengikuti' onClick={() => { handler('mengikuti') }}>Ruang Mu</Tab>
             </TabsHeader>
             <TabsBody className='min-h-[100vh] flex flex-col items-center w-full'>

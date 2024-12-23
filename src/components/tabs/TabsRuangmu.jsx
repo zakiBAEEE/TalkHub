2c77d184703ce2c4aad7d4aec7dd68998f696822
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-12-23 11:13:13 +0700

    Menambahkan Separator

--- src/components/tabs/TabsRuangmu.jsx
+++ src/components/tabs/TabsRuangmu.jsx
@@ -4,18 +4,25 @@ import { Tab, Tabs, TabsBody, TabsHeader } from '@material-tailwind/react'
 
 function TabsRuangmu({ handler, stateAktif }) {
     return (
-        <Tabs value={stateAktif} className=" m-auto mt-2 flex flex-col gap-4 items-start flex-grow w-[90%]">
-            <TabsHeader
-                className="rounded-none border-b border-blue-gray-50 bg-transparent p-0 gap-5 w-full flex flex-row justify-center items-center w-56"
-                indicatorProps={{
-                    className:
-                        "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",
-                }}
-            >
-                <Tab key='cari' value='cari' onClick={() => { handler('cari') }} className='w-full'>Mengikuti</Tab >
+        <Tabs value={stateAktif} className=" m-auto mt-6 flex flex-col gap-4 items-start flex-grow w-[90%]">
+            <div className="w-full">
+                <TabsHeader
+                    className="rounded-none border-b border-blue-gray-50 bg-transparent p-0 gap-5 flex flex-row justify-center items-center w-60"
+                    indicatorProps={{
+                        className:
+                            "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",
+                    }}
+                >
+                    <Tab key='cari' value='cari' onClick={() => { handler('cari') }} className='w-full'>
+                        Mengikuti
+                    </Tab>
 
-                <Tab key='mengikuti' value='mengikuti' onClick={() => { handler('mengikuti') }}>Ruang Mu</Tab>
-            </TabsHeader>
+                    <Tab key='mengikuti' value='mengikuti' onClick={() => { handler('mengikuti') }} className='w-full'>
+                        Ruang Mu
+                    </Tab>
+                </TabsHeader>
+                <div className="border-t border-blue-gray-700 mt-2"></div>
+            </div>
             <TabsBody className='min-h-[100vh] flex flex-col items-center w-full'>
                 {
                     stateAktif == 'cari' ? "" :

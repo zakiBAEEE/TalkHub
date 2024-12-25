60fe3c21551bc715178ab14eb6b509014b63c4be
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-12-25 17:26:47 +0700

    Inisiasi halaman profile

---
+++ src/pages/HalamanProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class HalamanProfile extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Detail Halaman Ruang Diskusi</h1>
+            </div>
+        )
+    }
+}
+export { HalamanProfile }

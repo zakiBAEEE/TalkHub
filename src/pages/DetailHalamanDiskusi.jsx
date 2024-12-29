57da4670b9a3e9982041f1eb3ff53f6b7f971135
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-12-29 10:31:07 +0700

    Membuat layout

--- src/pages/DetailHalamanDiskusi.jsx
+++ src/pages/DetailHalamanDiskusi.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 class DetailHalamanRuangDiskusi extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Detail Halaman Ruang Diskusi</h1>
+            <div className='flex flex-col  w-full h-full'>
+                <div className='flex-col'>
+                    <div className='w-2/3 bg-amber-700  h-[14rem] m-auto'></div>
+                    <div className='w-[12rem] bg-teal-600 h-[12rem] relative bottom-20 left-[18rem]'></div>
+                </div>
             </div>
         )
     }

980daabc2b0b7c2d522e192a54c1f1c4fea8fa54
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-12-29 09:12:03 +0700

    Membuat tombol Back

--- src/components/tombol/TombolFollow.jsx
+++ src/components/tombol/TombolFollow.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 function TombolFollow({ warna, label, handler }) {
-
+    return (
+        <Button color={warna} onClick={handler}>{label}</Button>
+    )
 }
 
 export { TombolFollow }

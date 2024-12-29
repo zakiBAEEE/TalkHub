1c1e3d254381f56b959bfe5321ded3a461e60005
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-12-29 11:30:58 +0700

    Menempatkan tombol follow

--- src/pages/DetailHalamanDiskusi.jsx
+++ src/pages/DetailHalamanDiskusi.jsx
@@ -11,7 +11,9 @@ class DetailHalamanRuangDiskusi extends React.Component {
                 </div>
                 <div className="w-2/3 bg-amber-700 h-[14rem] m-auto">
                     <div className="w-[12rem] bg-teal-600 h-[12rem] relative top-[9rem] left-[4rem]"></div>
-                    <TombolFollow warna="blue" label="Follow" handler={() => { }} />
+                    <div className='relative top-[3rem] flex justify-end'>
+                        <TombolFollow warna="blue" label="Follow" handler={() => { }} />
+                    </div>
                 </div>
             </div>
 

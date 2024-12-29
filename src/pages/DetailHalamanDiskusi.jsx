be6e068b6de9528736e34dc2a9b91e8a9f50a5b8
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-12-29 10:33:35 +0700

    Memperbaiki Layout

--- src/pages/DetailHalamanDiskusi.jsx
+++ src/pages/DetailHalamanDiskusi.jsx
@@ -5,8 +5,10 @@ class DetailHalamanRuangDiskusi extends React.Component {
         return (
             <div className='flex flex-col  w-full h-full'>
                 <div className='flex-col'>
-                    <div className='w-2/3 bg-amber-700  h-[14rem] m-auto'></div>
-                    <div className='w-[12rem] bg-teal-600 h-[12rem] relative bottom-20 left-[18rem]'></div>
+                    <div className='w-2/3 bg-amber-700  h-[14rem] m-auto'>
+                        <div className='w-[12rem] bg-teal-600 h-[12rem] relative top-[9rem] left-[2rem]'></div>
+                    </div>
+
                 </div>
             </div>
         )

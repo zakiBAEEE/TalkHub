22011166b1e62b7d7bf38e9a9215fc39228585b6
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2024-12-29 11:34:19 +0700

    hhahaha

--- src/components/tombol/TombolFollow.jsx
+++ src/components/tombol/TombolFollow.jsx
@@ -2,7 +2,7 @@ import { Button } from '@material-tailwind/react'
 import React from 'react'
 function TombolFollow({ warna, label, handler }) {
     return (
-        <Button color={warna} onClick={() => console.log("follow")}>{label}</Button>
+        <Button size='sm' color={warna} onClick={() => console.log("follow")} className='hover:bg-blue-700'>{label}</Button>
     )
 }
 

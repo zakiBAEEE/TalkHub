b297e4ad3fbb627b56a8ac7847a015f07bea5ba1
Muhammad Dzaky Raihan <muhammaddzakyraihan_2226250100@mhs.mdp.ac.id>
2025-01-21 20:32:17 +0700

    Menyederhanakan Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { HalamanBeranda } from './pages/HalamanBeranda'
 import { HalamanFollow } from './pages/HalamanFollow'
 import { HalamanDiskusi } from './pages/HalamanDiskusi';
 import { Routes, Route } from "react-router-dom";
-// eslint-disable-next-line no-unused-vars
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { DetailHalamanRuangDiskusi } from './pages/DetailHalamanDiskusi';
 import { HalamanProfile } from './pages/HalamanProfile';
 import { Sidebar } from './components/sidebar/Sidebar';
@@ -17,7 +16,7 @@ function App() {
 
   return (
     <div className='flex bg-blue-gray-50 min-h-[100vh]'>
-      <Sidebar toggleSidebar={toggleSidebar} isSidebarOn={isSidebarOn} />
+      <Sidebar toggleHandler={toggleSidebar} isSidebarOn={isSidebarOn} />
       < Routes >
         <Route path="/" element={<HalamanBeranda toggleSidebar={toggleSidebar} isSidebarOn={isSidebarOn} />} />
         <Route path="/diskusi" element={<HalamanDiskusi toggleSidebar={toggleSidebar} isSidebarOn={isSidebarOn} />} />
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,25 +1,76 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import { SidebarOn } from './SidebarOn';
-import { SidebarOf } from './SidebarOf';
 import PropTypes from 'prop-types';
+import { Card, ListItem, ListItemPrefix, Typography } from '@material-tailwind/react';
+import { Link } from 'react-router-dom';
+import { ChatBubbleBottomCenterIcon, HomeIcon, PowerIcon, UserCircleIcon, UserPlusIcon } from '@heroicons/react/24/solid';
 
-function Sidebar({ isSidebarOn, toggleSidebar }) {
+function Sidebar({ isSidebarOn, toggleHandler }) {
     return (
         <div className={`transition-all duration-900 ease-out ${isSidebarOn ? 'w-[12rem]' : 'w-20'
             }`}
         >
-            {isSidebarOn ? (
-                <SidebarOn toggleHandler={toggleSidebar} />
-            ) : (
-                <SidebarOf toggleHandler={toggleSidebar} />
-            )}
+            <Card className="h-[calc(100vh)] w-full max-w-[12rem] p-4 shadow-xl shadow-blue-gray-900/5 bg-white fixed">
+                <div className="mb-2 p-4">
+                    <button>
+                        <Typography variant="h5" color="blue-gray" onClick={toggleHandler}>
+                            {isSidebarOn ? 'TalkHub' : "T"}
+                        </Typography>
+                    </button>
+
+                </div>
+                <div className='flex flex-col gap-5'>
+                    <Link to="/">
+                        <ListItem>
+                            <ListItemPrefix>
+                                <HomeIcon className="h-5 w-5" />
+                            </ListItemPrefix>
+                            {isSidebarOn && 'Beranda'}
+                        </ListItem>
+                    </Link>
+                    <Link to="/follow">
+                        <ListItem>
+                            <ListItemPrefix>
+                                <UserPlusIcon className="h-5 w-5" />
+                            </ListItemPrefix>
+                            {isSidebarOn && 'Follow'}
+                        </ListItem>
+                    </Link>
+                    <Link to="/diskusi">
+                        <ListItem>
+                            <ListItemPrefix>
+                                <ChatBubbleBottomCenterIcon className="h-5 w-5" />
+                            </ListItemPrefix>
+                            {isSidebarOn && 'Diskusi'}
+                        </ListItem>
+                    </Link>
+
+                    <Link to="/profile">
+                        <ListItem>
+                            <ListItemPrefix>
+                                <UserCircleIcon className="h-5 w-5" />
+                            </ListItemPrefix>
+                            {isSidebarOn && 'Profile'}
+                        </ListItem>
+                    </Link>
+
+
+                    <ListItem>
+                        {/* <Link to="/"></Link> */}
+                        <ListItemPrefix>
+                            <PowerIcon className="h-5 w-5" />
+                        </ListItemPrefix>
+                        {isSidebarOn && 'Logout'}
+                    </ListItem>
+
+                </div>
+
+            </Card>
+
         </div>
     )
 }
 
 Sidebar.propTypes = {
     isSidebarOn: PropTypes.bool.isRequired,
-    toggleSidebar: PropTypes.func.isRequired
+    toggleHandler: PropTypes.func.isRequired
 }
 export { Sidebar }
